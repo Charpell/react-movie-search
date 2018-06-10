d65073188893815d98a33c27a21a104ecdf4c8be
Charpell <charpellumeh@gmail.com>
2018-06-10 09:43:18 +0100

    View a movie in the website

--- src/MovieRow.js
+++ src/MovieRow.js
@@ -2,6 +2,11 @@ import React, { Component } from 'react'
 
 
 class MovieRow extends Component {
+  viewMovie = () => {
+    const url = "https://www.themoviedb.org/movie/" + this.props.movie.id
+    window.location.href = url
+  }
+
   render() {
     const { movie: { id, poster_src, title, overview } } = this.props;
     
@@ -14,8 +19,9 @@ class MovieRow extends Component {
             </td>
 
             <td>
-            {title}
-            <p>{overview}</p>
+              <h3>{title}</h3>
+              <p>{overview}</p>
+              <input type="button" onClick={this.viewMovie} value="View"/>
             </td>
           </tr>
         </tbody>
